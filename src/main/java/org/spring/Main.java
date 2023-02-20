61a6c9cf7a37fd0efdcb6b9207d53054338f629f
joozepp <podelsky@gmail.com>
2023-02-20 19:06:47 +0200

    Init repo

---
+++ src/main/java/org/spring/Main.java
@@ -0,0 +1,14 @@
+package org.spring;
+
+
+/**
+ * author Joozepp
+ * @Date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
