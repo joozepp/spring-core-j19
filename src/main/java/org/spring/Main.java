b6b9b65e76cd952559b76e43da7bdce219276cbe
joozepp <podelsky@gmail.com>
2023-02-20 21:51:26 +0200

    Added Java config.

--- src/main/java/org/spring/Main.java
+++ src/main/java/org/spring/Main.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
  * @author Joozepp
  * @Date ${DATE}
- * @dogg
  */
 public class Main {
     public static void main(String[] args) {
